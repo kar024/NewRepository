a39008f8f8a7b433a22c9755792f43a72ffe8cde
kar024 <kar0241998@gmail.com>
2020-03-28 17:21:53 +0400

    LogIn SignUp

--- src/main/java/com/example/demo/LogInSignUp/Controllers/RegistrationController.java
+++ src/main/java/com/example/demo/LogInSignUp/Controllers/RegistrationController.java
@@ -8,14 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 public class RegistrationController {
     @Autowired
     private UserService userService;
